0571f00fea4a35d6815c705d16c03e6d81f6c305
ANDRE François <francois.andre@obs-mip.fr>
2015-06-22 14:17:56 +0200

    Fin exercice 3a

--- src/main/java/fr/jdev/atelier/client/MonApplication.java
+++ src/main/java/fr/jdev/atelier/client/MonApplication.java
@@ -1,14 +1,11 @@
 package fr.jdev.atelier.client;
 
-import org.gwtbootstrap3.client.ui.Container;
-import org.gwtbootstrap3.client.ui.Heading;
-import org.gwtbootstrap3.client.ui.Jumbotron;
-import org.gwtbootstrap3.client.ui.TextBox;
-import org.gwtbootstrap3.client.ui.constants.HeadingSize;
-
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.datepicker.client.DateBox;
 
@@ -21,32 +18,22 @@ public class MonApplication implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		Jumbotron jumbotron = new Jumbotron();
-		Container container = new Container();
-		Heading heading = new Heading(HeadingSize.H1);
-		heading.setText("Ma Todo List");
-		container.add(heading);
-		jumbotron.add(container);
-
-		VerticalPanel mainContent = new VerticalPanel();
-		mainContent.setWidth("100%");
+		DockLayoutPanel mainContent = new DockLayoutPanel(Unit.PX);
+		mainContent.getElement().getStyle().setMargin(5, Unit.PX);
+		mainContent.addNorth(new Label("Ma Todo List"), 25);
 		VerticalPanel taskForm = new VerticalPanel();
-
 		taskForm.setSpacing(5);
 		taskForm.add(new Label("Ma tâche"));
 		TextBox description = new TextBox();
-		description.setPlaceholder("Description");
+		description.setTitle("Description");
 		TextBox responsible = new TextBox();
-		responsible.setPlaceholder("Responsible");
+		responsible.setTitle("Responsible");
 		DateBox deadLine = new DateBox();
 		taskForm.add(description);
 		taskForm.add(responsible);
 		taskForm.add(deadLine);
-
-		mainContent.add(jumbotron);
 		mainContent.add(taskForm);
-
-		RootPanel.get().add(mainContent);
-
+		RootLayoutPanel.get().add(mainContent);
 	}
+
 }
